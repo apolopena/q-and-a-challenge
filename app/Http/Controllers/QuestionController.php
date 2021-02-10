e0ad20bfb36e6022b2a8a6a56d073400b6c98846
apolopena <penavilla@gmail.com>
2021-02-10 09:28:39 +0000

    📦 NEW: routes

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function index()
+    {
+        $questions = Question::all()
+            ->load('answers');
+
+        return $questions->toJson();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/questions', [QuestionController::class, 'index']);
+#Route::post('questions', 'QuestionController@store');
+#Route::post('questions/{id}', 'QuestionController@show');
+#Route::post('answers', 'AnswerController@store');
