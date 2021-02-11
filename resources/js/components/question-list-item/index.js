a83a721a4c58461ae958659b8525dbd1fc310281
apolopena <penavilla@gmail.com>
2021-02-11 20:35:29 +0000

    📦 NEW: component

---
+++ resources/js/components/question-list-item/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './style.css'
+
+const QuestionListItem = ({ item }) => (
+  <div>
+    I am a question list item
+  </div>
+)
+export default QuestionListItem
