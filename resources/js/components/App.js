ec36bd530a928e9fceca63d74a794f801afcaa8a
apolopena <penavilla@gmail.com>
2021-02-14 21:36:17 +0000

    🐛 FIX: scrollbar wiggles between react route changes

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import './App.css'
 import { Questions, Answers } from '../pages'
 
 class App extends Component {
