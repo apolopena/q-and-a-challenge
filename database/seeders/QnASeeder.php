03a04bf7b64de9bd57c0198372a0fea518ff3860
apolopena <penavilla@gmail.com>
2021-03-03 17:46:57 +0000

    🐛 FIX: db seeders

--- database/seeders/QnASeeder.php
+++ database/seeders/QnASeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\{Question, Answer};
 
 class QnASeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class QnASeeder extends Seeder
      */
     public function run()
     {
-        //
+        Answer::factory()->forQuestion()->count(2)->create();
+        Question::factory()->hasAnswers(3)->create();
+        Question::factory()->create();
+        Question::factory()->hasAnswers(5)->create();
     }
 }
